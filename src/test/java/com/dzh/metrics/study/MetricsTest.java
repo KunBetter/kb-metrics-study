692ba681ca87ef88465c7d6e212af4d539ee68ab
kunbetter <nk05xy@163.com>
2013-11-22 13:55:46 +0800

    test

---
+++ src/test/java/com/dzh/metrics/study/MetricsTest.java
@@ -0,0 +1,13 @@
+package com.dzh.metrics.study;
+
+public class MetricsTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
